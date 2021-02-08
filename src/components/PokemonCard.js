acd56f7cb73ebf7490b0c1ec7d3cd9b30524c066
Mitch Gilbert <mitchellpgilbert@gmail.com>
2021-02-08 15:45:29 -0800

    done

--- src/components/PokemonCard.js
+++ src/components/PokemonCard.js
@@ -2,20 +2,37 @@ import React from 'react'
 import { Card } from 'semantic-ui-react'
 
 class PokemonCard extends React.Component {
+
+  constructor() {
+    super() 
+
+    this.state = {
+      show: false
+    }
+  }
+  
+  handleClick = () => {
+    let flipShow = !this.state.show
+    this.setState({
+      show: flipShow
+    })
+  }
+
   render() {
     return (
-      <Card>
-        <div>
+      <Card onClick={() => this.handleClick()}>
+        <div> 
           <div className="image">
-            <img alt="oh no!" />
+            {this.state.show === false ? <img src={this.props.pokemon.sprites.front} alt="oh no!" /> : 
+            <img src={this.props.pokemon.sprites.back} />}
           </div>
           <div className="content">
-            <div className="header">POKEMON NAME HERE</div>
+            <div className="header">{this.props.pokemon.name}</div>
           </div>
           <div className="extra content">
             <span>
               <i className="icon heartbeat red" />
-              POKEMON HP HERE hp
+              {this.props.pokemon.hp}
             </span>
           </div>
         </div>
--- src/components/PokemonPage.js
+++ src/components/PokemonPage.js
@@ -5,16 +5,89 @@ import Search from './Search'
 import { Container } from 'semantic-ui-react'
 
 class PokemonPage extends React.Component {
+
+
+  state = {
+    allPokemon: [], 
+    inputValue: '',
+    pokemons: [],
+    name: '',
+    hp: '',
+    frontUrl: '',
+    backUrl: '',
+  }
+
+  componentDidMount() {
+    fetch('http://localhost:3000/pokemon')
+    .then(resp => resp.json())
+    .then(pokemon => this.setState({
+      allPokemon: pokemon,
+      pokemons: pokemon
+    }))
+  }
+
+  postPokemon = (formData) => {
+    let configObj = {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(formData)
+    }
+    fetch('http://localhost:3000/pokemon', configObj)
+    .then(resp => resp.json())
+    .then(this.setState({
+      allPokemon: [...this.state.allPokemon, formData]
+    }))
+  }
+
+  handleSearch = (event) => {
+    this.setState({
+      inputValue: event.target.value
+    })
+    this.filterPets()
+  }
+
+  filterPets = () => {
+    let search = this.state.allPokemon.filter(pokemon => {
+      return pokemon.name.toLowerCase().includes(this.state.inputValue.toLowerCase())
+    })
+    this.state.inputValue.length === 1 ? this.setState({allPokemon: this.state.pokemons}) :
+    this.setState({
+      allPokemon: search
+    })
+  }
+
+  handleChange = event => {
+    this.setState({
+      [event.target.name]: event.target.value
+    })
+  }
+
+  handleSubmit = event => {
+    event.preventDefault()
+    let formData = {
+      name: this.state.name,
+      hp: this.state.hp,
+      sprites: {
+      front: this.state.frontUrl,
+      back: this.state.backUrl
+      }
+    }
+    this.postPokemon(formData)
+  }
+  
   render() {
+    console.log(this.state.inputValue)
     return (
       <Container>
         <h1>Pokemon Searcher</h1>
         <br />
-        <PokemonForm />
+        <PokemonForm handleChange={this.handleChange} handleSubmit={this.handleSubmit} />
         <br />
-        <Search />
+        <Search handleSearch={this.handleSearch} />
         <br />
-        <PokemonCollection />
+        <PokemonCollection pokemon={this.state.allPokemon} inputValue={this.state.inputValue} />
       </Container>
     )
   }
